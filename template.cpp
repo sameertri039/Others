a5d2785a5544f27b27bb1efb747b3e32cf2c126c
Sameer Tripathi <96130532+sameertri039@users.noreply.github.com>
2022-12-04 11:17:19 -0800

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -17,15 +17,17 @@ template <typename T> istream &operator>> (istream &input, vector<T> &data) {for
 template <typename T> ostream &operator<< (ostream &output, const vector<T> &data) {for (const T &x : data) {output << x << " ";} output << "\n"; return output;}
 template <typename T, typename S> ostream& operator<< (ostream& output, const unordered_map<T, S>& v) {for (auto it : v) {output << it.first << ": " << it.second << "\n";} output << "\n"; return output;}
 template <typename T, typename S> ostream& operator<< (ostream& output, const map<T, S>& v) {for (auto it : v) {output << it.first << ": " << it.second << "\n";} output << "\n"; return output;}
-void _print(long long x) {cout << x << "\n";};
-void _print(string x) {cout << x << "\n";};
+void _print(long long x) {cout << x << "\n";}
+void _print(string x) {cout << x << "\n";}
 #define fastIO {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
 #define int long long
 #define nline "\n"
 #define all(x) x.begin(), x.end()
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+// replace less<int> with greater to get a decreasing ordered_set
+// replace less with less_equal<int> to also get duplicate elements in the set
+#define ordered_set tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
 
-const int MOD = 1e9 + 7, maxN = 1e6 + 5;
+const int MOD = 1e9 + 7, maxN = 1e6 + 5, inf = INT_MAX;
 
 
 void solve()
@@ -48,4 +50,3 @@ int32_t main()
 
 	return 0;
 }
-
